60ea438c37472633fefb62f4d474243c38b06c9b
amanoryoji <r.amano1212@cb-cloud.com>
2023-03-06 19:28:02 +0900

    add: pretieerの導入

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 120, // 自動折り返し文字数
+  tabWidth: 2, // タブサイズ
+  singleQuote: true, //	ダブルクォーテーション、シングルクォーテーションの選択
+  trailingComma: 'none', //	末尾のカンマ指定
+  semi: true //セミコロンの有無
+}
