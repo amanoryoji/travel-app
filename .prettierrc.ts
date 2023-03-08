65f6d1f1615282daced9478a07e2eb4d2fe089c6
amanoryoji <r.amano1212@cb-cloud.com>
2023-03-08 09:04:24 +0900

    fix: pretierrの修正

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -2,6 +2,6 @@ module.exports = {
   printWidth: 120, // 自動折り返し文字数
   tabWidth: 2, // タブサイズ
   singleQuote: true, //	ダブルクォーテーション、シングルクォーテーションの選択
-  trailingComma: 'none', //	末尾のカンマ指定
-  semi: true //セミコロンの有無
-}
+  trailingComma: "none", //	末尾のカンマ指定
+  semi: true, //セミコロンの有無
+};
